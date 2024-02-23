a17ef84c4769be9d83de64b167816735faa0c577
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2024-02-23 21:30:44 +0530

    templates.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const firstname = "Ravi";
-firstname = "Kumar";
+function greet(string, firstname, lastname) {
+  let lastnameType;
+  lastnameType = lastname === "Kumar" ? "short" : "big";
 
-console.log(firstname);
+  return `${string[0]} ${firstname} ${lastname},${string[1]} you have used your ${lastnameType} name`;
+}
+const firstname = "Ravi";
+const lastname = "Choudhary";
+const greeting = greet`Hello, ${firstname} ${lastname}`;
+console.log(greeting);
